4c3b24277ef2093a22c307342cffee2f8a0bbd5d
Anjasfedo <fedoaf98@gmail.com>
2024-06-05 03:01:44 +0700

    Do// Simplify The Code for Reusability

--- app.py
+++ app.py
@@ -1,90 +1,30 @@
 import os
 from flask import Flask, request, jsonify
-import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from PIL import Image
+from flask_cors import CORS
 import io
-from model_load import best_model, class_names
-# from firebase_admin import credentials
-# import firebase_admin 
-# from auth_middleware import firebase_authentication_middleware
-import datetime
-# from firebase_admin import firestore
-# from flask_cors import CORS
-# from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-# import matplotlib.pyplot as plt
+from model_load import model
 
 # Set the environment variable to disable oneDNN optimizations
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 app = Flask(__name__)
-# CORS(app)
-
-# cred = credentials.Certificate('serviceAccount.json')
-# firebase_admin.initialize_app(cred)
-
-# Fungsi untuk menyimpan hasil prediksi ke dalam Firestore
-# def save_prediction_to_firestore(predicted_class, confidence):
-#     db = firestore.client()
-    
-#     # Buat dokumen baru di koleksi 'history'
-#     new_prediction_ref = db.collection('history').document()
-    
-#     # Data untuk disimpan
-#     data = {
-#         'timestamp': datetime.datetime.now(),
-#         'predicted_class': predicted_class,
-#         'confidence': confidence
-#     }
-    
-#     # Simpan data ke dalam Firestore
-#     new_prediction_ref.set(data)
-
-# Fungsi untuk memuat dan memproses gambar
-def preprocess_image_as_array(image_file, show_output=True):
-    im = Image.open(image_file).convert('RGB')
-    im = im.resize((224, 224))
-
-    return np.asarray(im)
-
-def predict_image_class(model, img_array, class_names):
-    img_batch = np.expand_dims(img_array, axis=0)
-    predictions = model.predict(img_batch)
-    predicted_class_index = np.argmax(predictions, axis=1)[0]
-
-    if predicted_class_index < len(class_names):
-        predicted_class = class_names[predicted_class_index]
-        print(f"Predicted class: {predicted_class}")
-        return predicted_class
-    else:
-        print(f"Predicted class index {predicted_class_index} out of range for class names")
-        return None
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
-# @firebase_authentication_middleware
 def predict_image():
     try:
         if 'image' not in request.files:
             return jsonify({'error': 'Image data not found'}), 400
 
-        # Read image data from request
-        image_data = request.files['image']
+        image_data = request.files['image'] # The uploaded image
 
-        # Convert FileStorage to a file-like object
-        image_file = io.BytesIO(image_data.read())
+        image_file = io.BytesIO(image_data.read()) # Process this image
 
-        # Load and preprocess the image
-        img_array = preprocess_image_as_array(image_file, show_output=False)
+        print(model.summary()) # Show model summary, indicate the model load successfuly
+        
+        # Handle image preprocessing same as ipynb does, then do predict and return it
 
-        # Predict the image
-        predicted_class = predict_image_class(best_model, img_array, class_names)
-
-        if predicted_class is not None:
-            confidence = np.max(best_model.predict(np.expand_dims(img_array, axis=0))) * 100
-            # save_prediction_to_firestore(predicted_class, confidence)
-            return jsonify({'result': predicted_class, 'confidence': confidence}), 200
-        else:
-            return jsonify({'error': 'Failed to predict class for the image'}), 500
+        return jsonify({'result': 'lorem'}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
--- model_load.py
+++ model_load.py
@@ -1,34 +1,14 @@
 import os
 import tensorflow as tf
-# from google.cloud import storage 
-from pathlib import Path
 
-# Use os.path to construct the model path
+# Define the directory where the model is stored
 model_dir = "model"
-model_filename = "model_mm9.h5"
+
+# Define the filename of the model
+model_filename = "model.h5"
+
+# Construct the full path to the model file by joining the directory and filename
 model_path = os.path.join(model_dir, model_filename)
 
-# Load your model here
-# cloud_storage_url = "https://storage.cloud.google.com/hijaiyah_model/model_mm9.h5"
-# cloud_storage_url = "https://storage.cloud.google.com/hijaiyah_model/hijaiyah_final_model.h5"
-
-
-# client = storage.Client.from_service_account_json(Path('serviceAccount.json').resolve())
-# bucket = client.bucket("hijaiyah_model")
-# blob = bucket.blob(model_filename)
-
-# blob.download_to_filename(model_path)
-# Load your model directly from the cloud storage
-# best_model = None
-# try:
-best_model = tf.keras.models.load_model(model_path)
-# except Exception as e:
-#     best_model = None
-#     print(f"Error = {e}")
-
-# List of class names
-class_names = [ 'ain', 'alif', 'ba', 'dal', 'dhod', 'dzal',
-                'dzho', 'fa', 'ghoin', 'ha', 'ha\'', 'hamzah', 'jim',
-                'kaf', 'kho', 'lam', 'lamalif', 'mim', 'nun', 'qof',
-                'ro', 'shod', 'sin', 'syin', 'ta', 'tho', 'tsa',
-                'wawu', 'ya', 'zain']
+# Load the TensorFlow Keras model from the specified file path
+model = tf.keras.models.load_model(model_path)
