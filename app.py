d953be8998f51583fd55dbbe71f16b3f21777b1b
Anjasfedo <fedoaf98@gmail.com>
2024-06-15 14:49:33 +0700

    Do// Predict with DenseNet Model

--- app.py
+++ app.py
@@ -1,99 +1,60 @@
-from flask import Flask, request, jsonify, Response
-from flask_cors import CORS
-import tensorflow as tf
-import numpy as np
-import cv2
-import io
-from pathlib import Path
-from tensorflow.lite.python.interpreter import Interpreter
-
-# Disable oneDNN optimizations
 import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+from model_load import model
+
+# Set the environment variable to disable oneDNN optimizations
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 app = Flask(__name__)
 CORS(app)
 
-# Use pathlib to define paths
-model_dir = Path("model")
-tflite_model_path = model_dir / "detect.tflite"
-label_map_path = model_dir / "labelmap.txt"
+# Function to preprocess the image
 
-# Load the TFLite model into memory
-interpreter = Interpreter(model_path=str(tflite_model_path))
-interpreter.allocate_tensors()
+class_list = [
+    "Bunga Lawang", "Cengkeh", "Jahe", "Kayu Manis",
+    "Kemiri", "Kencur", "Ketumbar", "Kunyit", "Lengkuas", "Pala"
+]
 
-# Get model details
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2]
-float_input = (input_details[0]['dtype'] == np.float32)
-input_mean = 127.5
-input_std = 127.5
 
-# Load label map into memory
-with label_map_path.open('r') as f:
-    labels = [line.strip() for line in f.readlines()]
+def preprocess_image(img):
+    img = img.resize((416, 416))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = x / 255.0
+    return x
+
+
+# Function to make predictions
+def predict_image(model, img):
+    x = preprocess_image(img)
+    predictions = model.predict(x)
+    predicted_class_index = np.argmax(predictions)
+    predicted_class = class_list[predicted_class_index]
+    return predicted_class, predictions[0]
+
 
 @app.route('/predict', methods=['POST'])
-def predict_image():
+def predict():
     try:
         if 'image' not in request.files:
             return jsonify({'error': 'Image data not found'}), 400
 
         image_data = request.files['image']
-        image_file = io.BytesIO(image_data.read())
-        image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
+        img = Image.open(image_data)
+        img = img.convert('RGB')
 
-        # Preprocess the image
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        imH, imW, _ = image.shape
-        image_resized = cv2.resize(image_rgb, (width, height))
-        input_data = np.expand_dims(image_resized, axis=0)
+        predicted_class, predictions = predict_image(model, img)
+        confidence = float(np.max(predictions))
 
-        if float_input:
-            input_data = (np.float32(input_data) - input_mean) / input_std
-
-        # Run inference
-        interpreter.set_tensor(input_details[0]['index'], input_data)
-        interpreter.invoke()
-
-        # Retrieve detection results
-        boxes = interpreter.get_tensor(output_details[1]['index'])[0]
-        classes = interpreter.get_tensor(output_details[3]['index'])[0]
-        scores = interpreter.get_tensor(output_details[0]['index'])[0]
-
-        min_conf = 0.5  # Minimum confidence threshold for displaying results
-
-        for i in range(len(scores)):
-            if (scores[i] > min_conf) and (scores[i] <= 1.0):
-                ymin = int(max(1, (boxes[i][0] * imH)))
-                xmin = int(max(1, (boxes[i][1] * imW)))
-                ymax = int(min(imH, (boxes[i][2] * imH)))
-                xmax = int(min(imW, (boxes[i][3] * imW)))
-                object_name = labels[int(classes[i])]
-                confidence = scores[i]
-
-                # Draw bounding box
-                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
-                label = f'{object_name}: {int(confidence * 100)}%'
-                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-                label_ymin = max(ymin, labelSize[1] + 10)
-                cv2.rectangle(image, (xmin, label_ymin - labelSize[1] - 10), (xmin + labelSize[0], label_ymin + baseLine - 10), (255, 255, 255), cv2.FILLED)
-                cv2.putText(image, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-
-        # Convert the image to bytes
-        _, buffer = cv2.imencode('.jpg', image)
-        image_bytes = buffer.tobytes()
-        image_io = io.BytesIO(image_bytes)
-
-        return Response(image_io.getvalue(), mimetype='image/jpeg', headers={
-            'Content-Disposition': 'attachment; filename=predicted_image.jpg'
-        })
+        return jsonify({'class': predicted_class, 'confidence': round(confidence, 2)}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
